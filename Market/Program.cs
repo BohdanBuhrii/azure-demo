333c3bdc5b06e17f1158b3cdded4ee89a8d14cef
BohdanBuhrii <bobuhrii@gmail.com>
2023-03-07 09:09:41 +0200

    save notes

--- Market/Program.cs
+++ Market/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(5000); // to listen for incoming http connection on port 5001
+    options.ListenAnyIP(5000); // to listen for incoming http connection on port 5000
     //options.ListenAnyIP(7000, configure => configure.UseHttps()); // to listen for incoming https connection on port 7001
 });
 // Add services to the container.
